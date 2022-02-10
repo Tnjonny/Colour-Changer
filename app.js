12ffb5afc6661f9c43e66763a3ec8075ca345429
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-10 15:03:22 +0000

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const colours = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const colour = document.querrySelector('.colour');
+
+btn.addEventListener('click', function() {
+    
+    const randomNumber = 2;
+})
